1c5e15d81d14b9cdb1e3e95ff5db65a3660efac7
Saurabh Jaiswal <sjsaurabhjais@gmail.com>
2025-01-03 17:33:24 +0530

    Recent workout UI & HomeView Navigation

---
+++ FitnessApp/Model/Workout.swift
@@ -0,0 +1,18 @@
+//
+//  Workout.swift
+//  FitnessApp
+//
+//  Created by Saurabh Jaiswal on 03/01/25.
+//
+
+import SwiftUI
+
+struct Workout {
+    let id: Int
+    let title: String
+    let image: String
+    let tintColor: Color
+    let duration: String
+    let date: String
+    let calories: String
+}
