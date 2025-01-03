f6c212652f65d95a2516fd25e8aa4469050a0f06
Saurabh Jaiswal <sjsaurabhjais@gmail.com>
2025-01-03 12:54:18 +0530

    Initial Commit

---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by Saurabh Jaiswal on 03/01/25.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
