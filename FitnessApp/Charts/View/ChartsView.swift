0fb2967527e91efad4382e3dfab4c8934c398b82
Saurabh Jaiswal <sjsaurabhjais@gmail.com>
2025-01-09 11:00:19 +0530

    Chart options and MockedChartData

--- FitnessApp/Charts/View/ChartsView.swift
+++ FitnessApp/Charts/View/ChartsView.swift
@@ -8,8 +8,17 @@
 import SwiftUI
 import Charts
 
+enum ChartOptions: String, CaseIterable {
+    case oneWeek = "1W"
+    case oneMonth = "1M"
+    case threeMonths = "3M"
+    case yearToDate = "YTD"
+    case oneYear = "1Y"
+}
+
 struct ChartsView: View {
     @StateObject var viewModel = ChartsViewModel()
+    @State var selectedChart: ChartOptions = .oneWeek
     
     var body: some View {
         VStack {
@@ -19,14 +28,54 @@ struct ChartsView: View {
                 .frame(maxWidth: .infinity, alignment: .leading)
                 .padding()
             
-            Chart {
-                ForEach(viewModel.mockChartData) { data in
-                    BarMark(x: .value(data.date.formatted(), data.date, unit: .day), y: .value("Steps", data.count))
+            ZStack {
+                switch selectedChart {
+                case .oneWeek:
+                    Chart {
+                        ForEach(viewModel.mockChartData) { data in
+                            BarMark(x: .value(data.date.formatted(), data.date, unit: .day), y: .value("Steps", data.count))
+                        }
+                    }
+                case .oneMonth:
+                    Chart {
+                        ForEach(viewModel.mockOnemonthData) { data in
+                            BarMark(x: .value(data.date.formatted(), data.date, unit: .day), y: .value("Steps", data.count))
+                        }
+                    }
+                case .threeMonths:
+                    Chart {
+                        ForEach(viewModel.mockThreemonthData) { data in
+                            BarMark(x: .value(data.date.formatted(), data.date, unit: .day), y: .value("Steps", data.count))
+                        }
+                    }
+                case .yearToDate:
+                    EmptyView()
+                case .oneYear:
+                    Chart {
+                        ForEach(viewModel.mockChartData) { data in
+                            BarMark(x: .value(data.date.formatted(), data.date, unit: .day), y: .value("Steps", data.count))
+                        }
+                    }
                 }
+                
             }
             .foregroundColor(.green)
             .frame(maxHeight: 350)
             .padding(.horizontal)
+            
+            HStack {
+                ForEach(ChartOptions.allCases, id: \.rawValue) { option in
+                    Button(option.rawValue) {
+                        withAnimation {
+                            selectedChart = option
+                        }
+                    }
+                    .padding()
+                    .foregroundColor(selectedChart == option ? .white : .green)
+                    .background(selectedChart == option ? .green : .clear)
+                    .cornerRadius(10)
+                }
+            }
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
     }
--- FitnessApp/Charts/ViewModel/ChartsViewModel.swift
+++ FitnessApp/Charts/ViewModel/ChartsViewModel.swift
@@ -9,6 +9,10 @@ import Foundation
 
 class ChartsViewModel: ObservableObject {
     
+    @Published var mockOnemonthData = [DailyStepModel]()
+    @Published var mockThreemonthData = [DailyStepModel]()
+    
+    
     var mockChartData = [
         DailyStepModel(date: Date(), count: 15000),
         DailyStepModel(date: Calendar.current.date(byAdding: .day, value: -1, to: Date()) ?? Date(), count: 9775),
@@ -18,4 +22,25 @@ class ChartsViewModel: ObservableObject {
         DailyStepModel(date: Calendar.current.date(byAdding: .day, value: -5, to: Date()) ?? Date(), count: 7000),
         DailyStepModel(date: Calendar.current.date(byAdding: .day, value: -6, to: Date()) ?? Date(), count: 5000)
     ]
+    
+    init() {
+        var mockOneMonth = mockDataForDays(days: 30)
+        var mockThreeMonth = mockDataForDays(days: 90)
+        DispatchQueue.main.async {
+            self.mockOnemonthData = mockOneMonth
+            self.mockThreemonthData = mockThreeMonth
+        }
+    }
+    
+    
+    func mockDataForDays(days: Int) -> [DailyStepModel] {
+        var mockData = [DailyStepModel]()
+        for day in 0..<days {
+            let currentDate = Calendar.current.date(byAdding: .day, value: -day, to: Date()) ?? Date()
+            let randomStepCount = Int.random(in: 1000...15000)
+            let dailyStepData = DailyStepModel(date: currentDate, count: Double(randomStepCount))
+            mockData.append(dailyStepData)
+        }
+        return mockData
+    }
 }
